042c84576347b801f3c2fcf94966d1d31c8e57bb
dgd03146 <ibory1220@naver.com>
2024-02-28 18:16:35 +0000

    chore: set up lint & husky

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,35 +1,63 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path')
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json')
 
-/** @type {import("eslint").Linter.Config} */
+/*
+ * This is a custom ESLint configuration for use with
+ * Next.js apps.
+ *
+ * This config extends the Vercel Engineering Style Guide.
+ * For more information, see https://github.com/vercel/style-guide
+ *
+ */
+
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "prettier",
-    require.resolve("@vercel/style-guide/eslint/next"),
-    "eslint-config-turbo",
-  ],
+    '@vercel/style-guide/eslint/node',
+    '@vercel/style-guide/eslint/typescript',
+    '@vercel/style-guide/eslint/browser',
+    '@vercel/style-guide/eslint/react',
+    '@vercel/style-guide/eslint/next',
+    // turborepo custom eslint configuration configures the following rules:
+    //  - https://github.com/vercel/turbo/blob/main/packages/eslint-plugin-turbo/docs/rules/no-undeclared-env-vars.md
+    'eslint-config-turbo',
+  ].map(require.resolve),
+  parserOptions: {
+    project,
+  },
   globals: {
     React: true,
     JSX: true,
   },
-  env: {
-    node: true,
-    browser: true,
-  },
-  plugins: ["only-warn"],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
-        project,
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+
+        // Choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
+
+        // use <root>/path/to/folder/tsconfig.json
+        project: 'path/to/folder',
+
+        // Multiple tsconfigs (Useful for monorepos)
+
+        // use a glob pattern
+        project: 'packages/*/tsconfig.json',
+
+        // use an array
+        project: ['packages/module-a/tsconfig.json', 'packages/module-b/tsconfig.json'],
+
+        // use an array of glob patterns
+        project: ['packages/*/tsconfig.json', 'other-packages/*/tsconfig.json'],
       },
     },
   },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-  ],
-  overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
-};
+  ignorePatterns: ['node_modules/', 'dist/'],
+  // add rules configurations here
+  rules: {
+    'import/no-default-export': 'off',
+    'react/button-has-type': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    'no-html-link-for-pages': 0,
+  },
+}
